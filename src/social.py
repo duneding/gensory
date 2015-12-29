90225e2a909ec82d442fc4829ea3f7785cd91f26
Duneding <maj.dagostino@gmail.com>
2015-12-28 23:55:43 -0300

    opt social

--- src/social.py
+++ src/social.py
@@ -51,22 +51,23 @@ def tweetToJSON(tweet):
             }
 
 def userToJSON(user):
+
     return {
-                'created_at':user.created_at,
-                'description':user.description,
-                'favourites_count':user.favourites_count,
-                'followers_count':user.followers_count,
-                'friends_count':user.friends_count,
-                'id':user.id,
-                'lang':user.lang,
-                'listed_count':user.listed_count,
-                'location':user.location,
-                'name':user.name,
-                'profile_banner_url':user.profile_banner_url,
-                'profile_image_url':user.profile_image_url,
-                'screen_name':user.screen_name,
-                'statuses_count':user.statuses_count,
-                'url':user.url
+                'created_at': user.created_at,
+                'description': user.description,
+                'favourites_count': user.favourites_count,
+                'followers_count': user.followers_count,
+                'friends_count': user.friends_count,
+                'id': user.id,
+                'lang': user.lang,
+                'listed_count': user.listed_count,
+                'location': user.location,
+                'name': user.name,
+                #'profile_banner_url': str(user.profile_banner_url),
+                'profile_image_url': user.profile_image_url,
+                'screen_name': user.screen_name,
+                'statuses_count': user.statuses_count,
+                'url': user.url
             }
 
 def GetTweets(api, screen_name, since_id):
